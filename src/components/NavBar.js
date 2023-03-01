2cbd3ce687063758abc5d816afbb804563ecb826
Rajan Chaudhary <rajann44@gmail.com>
2023-03-01 22:53:53 +0100

    using states

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class NavBar extends Component {
+
+  render() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">React News</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">About</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+    )
+  }
+}
+
+export default NavBar
